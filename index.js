7d280d14da760d1765f6335318579a09a8848b47
Jeni Dang <jenipdang@yahoo.com>
2021-10-22 14:07:11 -0700

    commit tree 32fba99deac910224212126287723f039cc75b83

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+
+// const configObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// }
+
+// fetch('http://localhost:3000/dogs', configObject)
+//     .then(resp => resp.json())
+//     .then(object => console.log(object))
+//     .catch(function(error){
+//         alert("Bad things! Ragnarok!")
+//         console.log(error.message)
+//     })
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          name,
+          email
+        })
+      })
+      .then(response => response.json())
+      .then(function (object){
+        let p = document.createElement("p")
+        p.innerHTML = object.id
+        debugger
+        document.body.appendChild(p) 
+      })
+      // .then(function ( object ) {
+      //       document.body.innerHTML = object[ "id" ]
+      //   })
+      .catch( function ( error ) {
+            document.body.innerHTML = error.message
+        })
+  }
